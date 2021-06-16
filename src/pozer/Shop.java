47bfd61711f467bba15b72b03605a992e097599e
Lairon1 <62989407+Lairon1@users.noreply.github.com>
2021-06-16 22:56:34 +0300

    Add files via upload

--- src/pozer/Shop.java
+++ src/pozer/Shop.java
@@ -9,73 +9,59 @@ import org.bukkit.entity.Player;
 
 
 public class Shop implements CommandExecutor {
-	private Main plugin;
+
 	private Main main = (Main)Main.getPlugin(Main.class);
 
 	public Shop(Main main) {
-		this.plugin = main;
+
 	}
 
-
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		Player p = (Player)sender;
+		Player player = (Player)sender;
 		String near = new String();
 		if(args.length == 0) {
-			if(p.hasPermission("near.usage")) {
+			if(player.hasPermission("near.usage")) {
 				int rad = 0;
 				String ColourBracket = main.getConfig().getString("ColourBracket").replace("&", "§");
 				String ColourDistanse = main.getConfig().getString("ColourDistanse").replace("&", "§");
 				String Empty = main.getConfig().getString("Empty").replace("&", "§");
 				for(int radius = 1; radius <= 1000; radius++) {
-					if(p.hasPermission("near." + radius)) {
+					if(player.hasPermission("near." + radius)) {
 						rad = radius;
 					}
-
 				}
 				String NearMessage = main.getConfig().getString("NearMessage").replace("&", "§").replace("%RADIUS%", rad+"");
-				for(Player Online : Bukkit.getOnlinePlayers()) {
-					if(p.getWorld() == Online.getWorld()) {
-						if(distanse(p.getLocation(), Online.getLocation()) <= rad) {
-							if(p.getName().equalsIgnoreCase(Online.getName())) {
 
-							}else {
-								near = near + Online.getName()+ ColourBracket +"(" + ColourDistanse + distanse(p.getLocation(), Online.getLocation()) + ColourBracket  + ") " ;
-							}
+				for(Player Online : player.getWorld().getPlayers()) {
+					if(player.getLocation().distance(Online.getLocation()) <= rad) {
+						if(player != Online) {
+							near = near + Online.getName()+ ColourBracket +"(" + ColourDistanse + (int) player.getLocation().distance(Online.getLocation()) + ColourBracket  + ") " ;
 						}
 					}
 				}
-				if(near.equalsIgnoreCase("")) {
-					p.sendMessage(NearMessage + Empty);
+				if(near.isEmpty()) {
+					player.sendMessage(NearMessage + Empty);
 				}else {
-					p.sendMessage(NearMessage + near);
+					player.sendMessage(NearMessage + near);
 				}
 			}else {
 				String NoPerm = main.getConfig().getString("NoPerm").replace("&", "§");
-				p.sendMessage(NoPerm);
+				player.sendMessage(NoPerm);
 			}
 		}
-		if(args.length == 1) {
+		else {
 			if(args[0].equalsIgnoreCase("reload")) {
-				if(p.hasPermission("near.reload")) {
+				if(player.hasPermission("near.reload")) {
 					main.reloadConfig();
-					p.sendMessage("SuperNear: Перезагружен.");
+					player.sendMessage("SuperNear: Перезагружен.");
 				}else {
 					String NoPerm = main.getConfig().getString("NoPerm").replace("&", "§");
-					p.sendMessage(NoPerm);
+					player.sendMessage(NoPerm);
 				}
 			}
 		}
 		return false;
-	}
-	public int distanse(Location loc1, Location loc2) {
-		float Xentity = loc1.getBlockX();
-		float Yentity = loc1.getBlockY();
-		float Zentity = loc1.getBlockZ();
-		float Xdamager = loc2.getBlockX();
-		float Ydamager = loc2.getBlockY();
-		float Zdamager = loc2.getBlockZ();
-		float distanse = (float) Math.sqrt(Math.pow((Xentity - Xdamager), 2) + Math.pow((Yentity - Ydamager), 2) + Math.pow((Zentity - Zdamager), 2));
-		return (int) distanse;
 	}
 }
 
